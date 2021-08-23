8ff205310be336f3fbeac8cf592cb6e90ec4d500
Lukáš Cír <cir@cngroup.dk>
2021-08-23 18:23:29 +0200

    Initialization commit

---
+++ src/test/java/cz/ptw/databasebrowser/DatabaseBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.ptw.databasebrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseBrowserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
