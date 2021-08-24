4f3edbde0f5cfc7ccee26c35aa67dfe43e9b0fe0
Lukáš Cír <cir@cngroup.dk>
2021-08-24 07:48:46 +0200

    Testing testcontainers in github

--- src/test/java/cz/ptw/databasebrowser/DatabaseBrowserApplicationTests.java
+++ src/test/java/cz/ptw/databasebrowser/DatabaseBrowserApplicationTests.java
@@ -1,13 +1,39 @@
 package cz.ptw.databasebrowser;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.testcontainers.containers.MySQLContainer;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Slf4j
 @SpringBootTest
 class DatabaseBrowserApplicationTests {
 
+    @Rule
+    private MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.26");
+
+    @Autowired
+    private JdbcTemplate template;
+
+
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+            mysql.start();
+        DatabaseMetaData md = template.getDataSource().getConnection().getMetaData();
+        ResultSet rs = md.getTables(null, null, "%", null);
+        while (rs.next()) {
+            System.out.println(rs.getString(3));
+        }
     }
+
+
 
 }
